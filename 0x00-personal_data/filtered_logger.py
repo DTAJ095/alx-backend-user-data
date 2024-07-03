c1cfa089cc402165b222fbccc3a91b2477f1fc71
DTAJ095 <albanjaures26@gmail.com>
2024-07-03 19:24:55 +0100

    added

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,7 +34,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    
+
     return mysql.connector.connect(
         user=user,
         password=password,
@@ -48,15 +48,16 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    
+
     target_handler = logging.StreamHandler()
     target_handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(target_handler)
-    
+
     return logger
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """ Returns a log message """
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
@@ -71,15 +72,16 @@ def main() -> None:
     cursor.execute("SELECT COUNT(*) FROM users;")
     headers = [field[0] for field in cursor.description]
     logger = get_logger()
-    
+
     for row in cursor:
         data = ''
         for i, j in zip(row, headers):
             data += f'{j}={i}; '
         logger.info(data)
-    
+
     cursor.close()
     db.close()
+
 
 if __name__ == '__main__':
     main()
