d2ea45da7f4cc201eedb49b611d2e830d778a1c2
DTAJ095 <albanjaures26@gmail.com>
2024-07-03 23:11:32 +0100

    added

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,11 +3,11 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """ Returns a hashed password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def is_valid(hashed_password: str, password: str) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Validate a password """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
