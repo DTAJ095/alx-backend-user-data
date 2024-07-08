c1f644c7771a749be7ea157aa1fb028dc74f7614
DTAJ095 <albanjaures26@gmail.com>
2024-07-08 18:33:55 +0100

    added

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,18 +22,18 @@ if auth_type == 'basic_auth':
     auth = BasicAuth()
 
 
-# @app.before_request
-# def user_authentication():
-#     """ authenticate user before request """
-#     if auth:
-#         excluded_paths = ['/api/v1/status/',
-#                           '/api/v1/unauthorized/',
-#                           '/api/v1/forbidden/']
-#         if auth.require_auth(request.path, excluded_paths):
-#             if not auth.authorization_header(request):
-#                 abort(401)
-#             if not auth.current_user(request):
-#                 abort(403)
+@app.before_request
+def user_authentication():
+    """ authenticate user before request """
+    if auth:
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if not auth.authorization_header(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
 
 
 @app.errorhandler(401)
@@ -41,6 +41,13 @@ def unauthorized(error) -> str:
     """ unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> None:
       - unauthorized status
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      - forbidden status
+    """
+    abort(403)
