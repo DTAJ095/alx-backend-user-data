b0873b75c1d331f2fb9f7770f1eeb95f76939a1f
DTAJ095 <albanjaures26@gmail.com>
2024-07-10 19:32:09 +0100

    added

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,6 +11,7 @@ class SessionAuth(Auth):
     """ Session Auth class
     """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ Create a session
         """
@@ -19,3 +20,11 @@ class SessionAuth(Auth):
         session_id = uuid.uuid4()
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self,
+                               session_id: str = None) -> str:
+        """ User ID for Session ID
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
