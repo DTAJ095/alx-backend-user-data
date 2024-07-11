b8fd0243640db99d8e59eb72ff13d076fdd87d5b
DTAJ095 <albanjaures26@gmail.com>
2024-07-12 00:12:44 +0100

    updated views

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -42,5 +42,5 @@ def user_auth_session_logout() -> Union[bool, Dict]:
     """ Delete user session/logout """
     from api.v1.app import auth
     if not auth.destroy_session(request):
-        abort(404)
+        return False, abort(404)
     return jsonify({}), 200
