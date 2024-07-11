83f8009276612b577678299c033539cc77529229
DTAJ095 <albanjaures26@gmail.com>
2024-07-11 23:11:09 +0100

    updated views

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -15,12 +15,12 @@ def user_auth_session_login():
     email = request.form.get('email')
     password = request.form.get('password')
     if email is None or email == "":
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if password is None or password == "":
-        return jsonify({ "error": "password missing" }), 400
-    user = User.search({ email: "email" })
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({email: "email"})
     if user is None:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     if user.is_valid_password(password):
         from api.v1.app import auth
         session_id = auth.create_session(user_id=user.id)
@@ -29,4 +29,4 @@ def user_auth_session_login():
         response.set_cookie(cookie_name, session_id)
         return response
     else:
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
