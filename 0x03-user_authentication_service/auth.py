61fe0c58f69cb75775d53a70b92faa951d5fd54f
DTAJ095 <albanjaures26@gmail.com>
2024-07-16 12:54:11 +0100

    added

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" Hash password """
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
+from typing import Union
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a user """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validate a user login """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def _generate_uuid(self) -> str:
+        """ Generate a UUID """
+        return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        """ Create a new session """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """ Get a user from a session ID """
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy a session """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
+def _hash_password(password: str) -> bytes:
+    """ Hash a password """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
