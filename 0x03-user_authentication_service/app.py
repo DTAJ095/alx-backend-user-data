28f49ffb7e7de1ff5dc0fc405258bc10d0bc19ed
DTAJ095 <albanjaures26@gmail.com>
2024-07-16 14:38:58 +0100

    updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,48 +15,48 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'], strict_slashes=False)
-def register_user():
-    """ user registration method """
-    email = request.form.get('email')
-    password = request.form.get('password')
+# @app.route('/users', methods=['POST'], strict_slashes=False)
+# def register_user():
+#     """ user registration method """
+#     email = request.form.get('email')
+#     password = request.form.get('password')
 
-    try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-
-
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login():
-    """ user login method """
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    if AUTH.valid_login(email, password):
-        session_id = AUTH.create_session(email)
-        response = make_response(jsonify({"email": email,
-                                          "message": "logged in"}))
-        response.set_cookie("session_id", session_id)
-        return response
-    else:
-        abort(401)
+#     try:
+#         user = AUTH.register_user(email, password)
+#         return jsonify({"email": email, "message": "user created"})
+#     except ValueError:
+#         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout():
-    """ user logout method """
-    session_id = request.cookies.get("session_id")
-    if not session_id:
-        abort(403)
-    user = AUTH.get_user_from_session_id(session_id)
+# @app.route('/sessions', methods=['POST'], strict_slashes=False)
+# def login():
+#     """ user login method """
+#     email = request.form.get('email')
+#     password = request.form.get('password')
 
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect(url_for('index'))
-    else:
-        abort(403)
+#     if AUTH.valid_login(email, password):
+#         session_id = AUTH.create_session(email)
+#         response = make_response(jsonify({"email": email,
+#                                           "message": "logged in"}))
+#         response.set_cookie("session_id", session_id)
+#         return response
+#     else:
+#         abort(401)
+
+
+# @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+# def logout():
+#     """ user logout method """
+#     session_id = request.cookies.get("session_id")
+#     if not session_id:
+#         abort(403)
+#     user = AUTH.get_user_from_session_id(session_id)
+
+#     if user:
+#         AUTH.destroy_session(user.id)
+#         return redirect(url_for('index'))
+#     else:
+#         abort(403)
 
 
 if __name__ == "__main__":
