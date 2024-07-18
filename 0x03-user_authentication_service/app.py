7a58af9e7aa3cf3d5bc550158dcd1da089456fd3
DTAJ095 <albanjaures26@gmail.com>
2024-07-18 19:48:53 +0100

    updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -36,8 +36,9 @@ def login():
 
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
-        response = make_response(jsonify({"email": email,
-                                          "message": "logged in"}))
+        response = make_response(
+            jsonify({"email": email, "message": "logged in"})
+            )
         response.set_cookie("session_id", session_id)
         return response
     else:
